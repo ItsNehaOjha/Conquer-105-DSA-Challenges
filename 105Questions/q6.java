4ff809c5d0878f661b6f2f9fcf6dc291ec0d2c39
ItsNehaOjha <ojhaneha0028@gmail.com>
2024-04-29 08:49:17 +0530

    question 6

---
+++ 105Questions/q6.java
@@ -0,0 +1,18 @@
+class q6{
+    public static void main(String[] args) {
+        // Left Rotate the Array by One
+        // NOTE: make sure in new array you are putting the rotated elements
+
+        int[] arr= {1, 2, 3, 4, 5};
+        int n=arr.length;
+        int[] num=new int[n];
+        for(int i=0;i<n;i++){
+            num[i]=arr[(i+1)%n];
+        }
+        for(int i=0;i<n;i++){
+            arr[i]= num[i];        // NOTE that int the previous array we have to put the new changed element 
+            System.out.print(num[i] +" ");
+        }
+        
+    }
+}
